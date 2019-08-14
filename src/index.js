d55c911ca27621697f604fc9169daa16a5ce52e3
Mihir Chaturvedi <plibither8@gmail.com>
2019-08-15 00:04:00 +0530

    Fix: A lot of stuff, again

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 const projectCards = document.querySelectorAll('section.projects .card');
 
 for (const card of projectCards) {
+	const para = card.querySelector('p.details');
 	const button = card.querySelector('button');
-	const ellipsis = card.querySelector('span.ellipsis');
-	const hidden = card.querySelector('span.hidden');
 
 	button.addEventListener('click', () => {
-		ellipsis.remove();
-		button.remove();
-		hidden.classList.remove('hidden');
+		para.innerHTML = para.dataset.detailsFull;
 	});
 }
