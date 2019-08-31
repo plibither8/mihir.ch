05f446504521f246e9a6dd70b49561d91f670d6b
Mihir Chaturvedi <plibither8@gmail.com>
2019-09-01 01:21:16 +0530

    Fix: Comment JS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+// Expand the "Psst! ++" button
 const introExpandButton = document.querySelector('section.intro button.expand');
 introExpandButton.addEventListener('click', () => {
 	introExpandButton.nextElementSibling.classList.remove('nodisplay');
 	introExpandButton.remove();
 })
 
+// Expand/collapse projects' "++" buttons
 const projectCards = document.querySelectorAll('section.projects .card');
-
 for (const card of projectCards) {
 	const details = card.querySelector('p.details');
 	const button = card.querySelector('button');
