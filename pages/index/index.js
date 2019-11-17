884b11dcd8697169a94cecb927eddc2880936f18
Mihir Chaturvedi <plibither8@gmail.com>
2019-11-17 12:58:11 +0530

    Fix: Add parent toggling button to projects section

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,15 +10,36 @@ psstButton.addEventListener('click', () => {
 });
 
 // Expand/collapse projects' "++" buttons
-const projectCards = document.querySelectorAll('#projects .card');
-for (const card of projectCards) {
-	const details = card.querySelector('div.details');
-	const button = card.querySelector('button');
+function toggleProjectVisibility(project, collapse) {
+	const details = project.querySelector('div.details');
+	const button = project.querySelector('button');
+	// 0 -> Expand, 1 -> Collapse
+	const action = collapse == null ? !project.classList.contains('card--collapsed') : collapse;
+	if (action) {
+		project.classList.add('card--collapsed');
+		button.innerHTML = '++';
+	} else {
+		project.classList.remove('card--collapsed');
+		button.innerHTML = '--';
+	}
+}
 
-	button.addEventListener('click', () => {
-		details.classList.toggle('nodisplay');
-		button.innerHTML = button.innerHTML == '++' ? '--' : '++';
-	});
+const projectCards = [...document.querySelectorAll('#projects .card')];
+
+// Parent button controlling all projects
+const projectsSection = document.querySelector('#projects');
+const projectsParentButton = projectsSection.querySelector('button.parent-controller');
+projectsParentButton.addEventListener('click', () => {
+	const action = !projectsSection.classList.contains('--collapsed');
+	projectsSection.classList.toggle('--collapsed');
+	projectCards.map(project => toggleProjectVisibility(project, action));
+	projectsParentButton.innerHTML = action ? '++' : '--';
+});
+
+// Individual project card buttons
+for (const project of projectCards) {
+	const button = project.querySelector('button');
+	button.addEventListener('click', () => toggleProjectVisibility(project));
 }
 
 // Activity
