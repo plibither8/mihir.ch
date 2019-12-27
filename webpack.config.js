be0d2117b37a4274cbed8a1045301d201ad719d0
Mihir Chaturvedi <plibither8@gmail.com>
2019-12-27 22:13:59 +0530

    Fix: Download, save and use remote index yml

--- webpack.config.js
+++ webpack.config.js
@@ -16,36 +16,9 @@ const WriteFilePlugin = require('write-file-webpack-plugin');
 // Other modules
 const fetch = require('node-fetch');
 
-// Get all that YAML data
-const data = require('./scripts/get-yaml-data');
-
 // Get all page roots
 const pagesDir = path.join(__dirname, 'pages/');
 const pages = readdirSync(pagesDir).filter(f => statSync(path.join(pagesDir, f)).isDirectory());
-const pagesToExclude = ['404', 'index'];
-
-// Site paths for sitemap generator - exclude some pages
-const sitemapPaths = [
-	'/',
-	...pages.filter(p => !pagesToExclude.includes(p)).map(p => '/' + p)
-];
-
-// webpack entry object
-const entryObj = {};
-pages.map(page => {
-	entryObj[page] = `./pages/${page}/${page}`
-});
-
-// HTML Webpack plugin generator
-const htmlPlugins = pages
-	.filter(page => page !== 'index')
-	.map(page => new HtmlWebpackPlugin({
-			template: `./pages/${page}/${page}.pug`,
-			navigation: data.navigation,
-			filename: page + '.html',
-			inject: false
-		})
-	);
 
 // get recent-activity.json
 const getRecentActivity = async () => {
@@ -59,71 +32,105 @@ const getRecentActivity = async () => {
 	};
 }
 
-module.exports = async (env, argv) => ({
-	devtool: 'sourcemap',
-	devServer: {
-		contentBase: [
-			'./dist',
-			'./'
-		],
-		hot: true,
-		writeToDisk: true,
-		watchContentBase: true
-	},
-	stats: 'errors-only',
-	entry: entryObj,
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'assets/js/[name].js',
-		publicPath: '',
-	},
-	module: {
-		rules: [{
-			test: /\.pug/,
-			use: [{
-				loader: 'pug-loader'
-			}]
-		}]
-	},
-	plugins: [
-		new SizePlugin(),
-		new WriteFilePlugin(),
-		new CopyWebpackPlugin([
-			{
-				from: 'assets',
-				to: 'assets'
-			}, {
-				from: 'static',
-				to: ''
-			}
-		]),
-		new HtmlWebpackPlugin({
-			template: './pages/index/index.pug',
-			projects: data.projects,
-			networks: data.networks,
-			navigation: data.navigation,
-			activity: await getRecentActivity(),
-			filename: 'index.html',
-			inject: false
-		}),
-		...htmlPlugins,
-		new ImageminPlugin(),
-		new ImageminWebpWebpackPlugin({
-			config:[
-				{
-					test: /assets\/img\/projects/
-				}
+module.exports = async (env, argv) => {
+	// Download project-index file and save
+	await require('./scripts/download-project-index')();
+	// Get all that YAML data
+	const data = require('./scripts/get-yaml-data');
+
+	// Site paths for sitemap generator - exclude some pages
+	const sitemapExcludes = ['404', 'index'];
+	const sitemapPaths = [
+		'/',
+		...pages.filter(p => !sitemapExcludes.includes(p)).map(p => '/' + p)
+	];
+
+	// Webpack entry object
+	const entryObj = {};
+	pages.map(page => {
+		entryObj[page] = `./pages/${page}/${page}`
+	});
+
+	// HTML Webpack plugin generator
+	const htmlPluginBaseOptions = page => ({
+		template: `./pages/${page}/${page}.pug`,
+		navigation: data.navigation,
+		filename: page + '.html',
+		inject: false
+	});
+
+	const htmlPluginExcludes = ['index', 'projects']
+	const htmlPlugins = pages
+		.filter(page => !htmlPluginExcludes.includes(page))
+		.map(page => new HtmlWebpackPlugin(htmlPluginBaseOptions(page)));
+
+	return {
+		devtool: 'sourcemap',
+		devServer: {
+			contentBase: [
+				'./dist',
+				'./'
 			],
-			detailedLogs: true
-		}),
-		new SitemapPlugin('https://mihir.ch', sitemapPaths)
-	],
-	resolve: {
-		extensions: [
-			'.js'
-		]
-	},
-	optimization: {
-		concatenateModules: true,
+			hot: true,
+			writeToDisk: true,
+			watchContentBase: true
+		},
+		stats: 'errors-only',
+		entry: entryObj,
+		output: {
+			path: path.join(__dirname, 'dist'),
+			filename: 'assets/js/[name].js',
+			publicPath: '',
+		},
+		module: {
+			rules: [{
+				test: /\.pug/,
+				use: [{
+					loader: 'pug-loader'
+				}]
+			}]
+		},
+		plugins: [
+			new SizePlugin(),
+			new WriteFilePlugin(),
+			new CopyWebpackPlugin([
+				{
+					from: 'assets',
+					to: 'assets'
+				}, {
+					from: 'static',
+					to: ''
+				}
+			]),
+			new HtmlWebpackPlugin({
+				...htmlPluginBaseOptions('index'),
+				projects: data.projects,
+				networks: data.networks,
+				activity: await getRecentActivity()
+			}),
+			new HtmlWebpackPlugin({
+				...htmlPluginBaseOptions('projects'),
+				projectIndex: data.projectIndex
+			}),
+			...htmlPlugins,
+			new ImageminPlugin(),
+			new ImageminWebpWebpackPlugin({
+				config:[
+					{
+						test: /assets\/img\/projects/
+					}
+				],
+				detailedLogs: true
+			}),
+			new SitemapPlugin('https://mihir.ch', sitemapPaths)
+		],
+		resolve: {
+			extensions: [
+				'.js'
+			]
+		},
+		optimization: {
+			concatenateModules: true,
+		}
 	}
-});
+};
