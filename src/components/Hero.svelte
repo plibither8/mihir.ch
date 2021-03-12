bd27edf83b2f20e3cc53b632c8c54bbb84cb21ad
Mihir Chaturvedi <plibither8@gmail.com>
2021-03-12 20:26:23 +0530

    Fix: Ready for simpler graphics

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,33 +1,23 @@
-<script>
-  import Graphic from './Graphic.svelte';
-</script>
+<section class='border-b-8 border-gray-800 p-7 md:px-10 md:py-20'>
+  <h1 class='sticky mb-40 text-4xl font-semibold leading-normal text-gray-900 center md:text-6xl top-7 md:top-20'>
+    Mihir Chaturvedi.
+  </h1>
 
-<section>
-  <div class='h-screen p-7 md:p-10 text-container'>
-    <h1 class='sticky mb-40 text-4xl font-bold leading-normal text-white md:text-6xl top-7 md:top-10'>
-      Mihir Chaturvedi.
-    </h1>
-  
-    <h2 class='sticky mb-20 text-xl leading-normal text-gray-300 md:text-2xl top-1/2'>
-      ...or perhaps, <strong>plibither8</strong>.
-    </h2>
-  
-    <h2 class='sticky text-2xl font-bold leading-normal text-gray-100 md:text-4xl top-full'>
-      Frontend and UX Developer.
-    </h2>
-  </div>
+  <h2 class='mb-8 text-xl leading-normal text-gray-700 center md:text-2xl'>
+    ...or perhaps, <strong class='font-semibold'>plibither8</strong>.
+  </h2>
 
-  <Graphic />
+  <h2 class='text-2xl leading-normal text-gray-800 center font-regular md:text-4xl'>
+    Frontend and UX Developer.
+  </h2>
 </section>
 
 <style lang='scss'>
-  .text-container {
-    height: 90vh;
+  section {
+    background: #f7f7b4;
+  }
 
-    // padding: 5vw;
-
-    // h1 {
-    //   top: 5vw;
-    // }
+  .center {
+    @apply max-w-5xl mx-auto;
   }
 </style>
